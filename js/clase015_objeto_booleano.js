2f03d331f8649d82bf18baf2effd0787244a3d15
searito <searclimaco@gmail.com>
2018-04-10 21:38:36 -0600

    Objeto Booleano

---
+++ js/clase015_objeto_booleano.js
@@ -0,0 +1,13 @@
+/*
+    UN BOOLEANO SERA FALSE, CUANDO TENGA COMO VALOR 0, -0, false, NAN, null, undefined, String Vacío
+*/
+
+/*
+    UN BOOLEANO SERA TRUE, CUANDO TENGA NUMEROS A EXCECION DEL 0, LETRAS, ETC
+*/
+
+var booleano = true;
+
+if (booleano) {
+    console.error("Me He Impreso...");
+}
