99c25ea3733eeda37a50aa1d15abecbcf4f9f748
searito <searclimaco@gmail.com>
2018-04-16 12:21:17 -0600

    Switch condicional multiple

---
+++ js/clase025_switch.js
@@ -0,0 +1,77 @@
+var mes = 9;
+
+switch (mes) {
+    case 1:
+        console.log("Enero");
+        break;
+
+    case 2:
+        console.log("Febrero");
+        break;
+
+    case 3:
+        console.log("Marzo");
+        break;
+
+    case 4:
+        console.log("Abril");
+        break;
+
+    case 5:
+        console.log("Mayo");
+        break;
+
+    case 6:
+        console.log("Junio");
+        break;
+
+    case 7:
+        console.log("Julio");
+        break;
+
+    case 8:
+        console.log("Agosto");
+        break;
+
+    case 9:
+        console.log("Septiembre");
+        break;
+
+    case 10:
+        console.log("Octubre");
+        break;
+
+    case 11:
+        console.log("Noviembre");
+        break;
+
+    case 12:
+        console.log("Diciembre");
+        break;
+
+    default:
+        console.log("No Es Un Mes");
+        break;
+}
+
+
+
+
+var cond = 40;
+
+switch (cond) {
+    case (10 > 5) ? 40:
+        1:
+            console.log("10 Es Mayor");
+        break;
+    case 2:
+        console.log("5 Es Mayor");
+        break;
+
+    case 3:
+        console.log("Ninguno Es Mayor");
+        break;
+    default:
+        console.log("Ninguna Condicion Se Cumple");
+        break;
+}
